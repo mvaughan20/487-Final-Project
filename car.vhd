1fcb2b68e74e79340b1b904402857fc7cb9e6922
shadykamel <94701716+shadykamel@users.noreply.github.com>
2023-12-15 18:19:53 -0500

    Update car.vhd
    Updated to draw car

--- car.vhd
+++ car.vhd
@@ -8,7 +8,7 @@ ENTITY car is
         v_sync : IN STD_LOGIC;
         pixel_row : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
         pixel_col : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
-        car_x : IN STD_LOGIC_VECTOR (10 DOWNTO 0); -- current car x position
+        car_x_pos : IN STD_LOGIC_VECTOR (10 DOWNTO 0); -- current car x position
         start : IN STD_LOGIC; -- initiates start
         red : OUT STD_LOGIC;
         green : OUT STD_LOGIC;
@@ -17,39 +17,79 @@ ENTITY car is
 END car;
 
 ARCHITECTURE behavioral of car is
-    CONSTANT bsize : INTEGER := 8;
+    CONSTANT car_body_width : INTEGER := 50;
+    CONSTANT car_body_height : INTEGER := 18;
+    CONSTANT car_top_width : INTEGER := 23;
+    CONSTANT car_top_height : INTEGER := 27;
+    CONSTANT wheel_size : INTEGER := 12;
     CONSTANT inactive_street_width : INTEGER := 200; -- width of street will be approximately 2/3 of 800px
     CONSTANT inactive_street_height : INTEGER := 100;
     CONSTANT active_street_width : INTEGER := 100;
     CONSTANT active_street_height : INTEGER := 50;
     CONSTANT win_street_width : INTEGER := 50;
     CONSTANT win_street_height : INTEGER := 100;
-    SIGNAL ball_on : STD_LOGIC;
+    SIGNAL car_on : STD_LOGIC;
     SIGNAL inactive_street_on : STD_LOGIC;
     SIGNAL active_street_on : STD_LOGIC;
     SIGNAL win_street_on : STD_LOGIC;
-    
-    SIGNAL ball_x : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
-    SIGNAL ball_y : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(300, 11);
+    SIGNAL vx, vy : STD_LOGIC_VECTOR (10 DOWNTO 0); 
+    SIGNAL vx1, vy1 : STD_LOGIC_VECTOR (10 DOWNTO 0); 
+    SIGNAL car_x : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
+    SIGNAL car_y : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(300, 11);
 BEGIN
-    red <= NOT ball_on; -- color setup for blue car on green background
-	green <= NOT inactive_street_on AND NOT active_street_on AND NOT win_street_on AND NOT ball_on;
-	blue  <= ball_on;
-	-- process to draw ball current pixel address is covered by ball position
-	bdraw : PROCESS (ball_x, ball_y, pixel_row, pixel_col) IS
+    red <= NOT car_on; -- color setup for blue car on green background
+	green <= NOT inactive_street_on AND NOT active_street_on AND NOT win_street_on AND NOT car_on;
+	blue  <= car_on;
+	cdraw : PROCESS (car_x, car_y, pixel_row, pixel_col) IS
 	BEGIN
-	   IF start = '0' THEN
-		IF (pixel_col >= ball_x - bsize) AND
-		 (pixel_col <= ball_x + bsize) AND
-			 (pixel_row >= ball_y - bsize) AND
-			 (pixel_row <= ball_y + bsize) THEN
-				ball_on <= '1';
+	   IF start = '1' THEN
+		IF (pixel_col >= car_x - car_body_width) AND
+		 (pixel_col <= car_x + car_body_width) AND
+			 (pixel_row >= car_y - car_body_height) AND
+			 (pixel_row <= car_y + car_body_height) THEN
+				car_on <= '1';
+	    ELSIF (pixel_col >= car_x - car_top_width) AND
+		       (pixel_col <= car_x + car_top_width) AND
+			     (pixel_row >= (car_y - 19) - car_top_height) AND
+		  	        (pixel_row <= (car_y - 19) + car_top_height) THEN
+				car_on <= '1';
+		ELSIF((vx * vx) + (vy * vy)) < (wheel_size * wheel_size) THEN -- test if radial distance < bsize
+            car_on <= '1';
+        ELSIF((vx1 * vx1) + (vy1 * vy1)) < (wheel_size * wheel_size) THEN -- test if radial distance < bsize
+            car_on <= '1';
 		ELSE
-			ball_on <= '0';
+			car_on <= '0';
 		END IF;
-		
+            
 		END IF;
-	END PROCESS bdraw;
+	END PROCESS cdraw;
+	
+	wheel : PROCESS (car_x, car_y, pixel_row, pixel_col) IS 
+	BEGIN
+	   IF pixel_col <= car_x - 25 THEN -- vx = |ball_x - pixel_col|
+            vx <= (car_x - 25) - pixel_col;
+        ELSE
+            vx <= pixel_col - (car_x - 25);
+        END IF;
+        IF pixel_row <= car_y + 20 THEN -- vy = |ball_y - pixel_row|
+            vy <= (car_y + 20) - pixel_row;
+        ELSE
+            vy <= pixel_row - (car_y +20);
+        END IF;
+        
+        
+        
+        IF pixel_col <= car_x + 25 THEN -- vx = |ball_x - pixel_col|
+            vx1 <= (car_x + 25) - pixel_col;
+        ELSE
+            vx1 <= pixel_col - (car_x + 25);
+        END IF;
+        IF pixel_row <= car_y + 20 THEN -- vy = |ball_y - pixel_row|
+            vy1 <= (car_y + 20) - pixel_row;
+        ELSE
+            vy1 <= pixel_row - (car_y +20);
+        END IF;
+	END PROCESS wheel;
 	
 	inactive_street_draw : PROCESS IS
 	BEGIN
